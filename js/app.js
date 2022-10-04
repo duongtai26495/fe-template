26a5369250da4848f83f6c8c238791743a44a522
duongtai26495 <taiduong.264@gmail.com>
2022-10-04 18:46:24 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+function openMenuBar(){
+    document.getElementById("side-bar").style.left = "0%"
+}
+
+function closeSideBar(){
+   document.getElementById("side-bar").style.left = "-100%"
+}
+
+function closeUserBar(){
+    document.getElementById("user-infor-tab").style.right = "-100%";
+}
+
+function openUserBar(){
+    document.getElementById("user-infor-tab").style.right = "0%";
+}
