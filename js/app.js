65d8eefa0b4c286b29b4ee4166b16d1d033e82c4
taiduong.laptop <taiduong.264@gmail.com>
2022-10-05 08:41:23 +0700

    update

--- js/app.js
+++ js/app.js
@@ -14,3 +14,30 @@ function closeUserBar(){
 function openUserBar(){
     document.getElementById("user-infor-tab").style.right = "0%";
 }
+
+function uploadImage(file){
+    if(file.files && file.files[0]){
+        if(file.files[0].size > 500000){
+            document.getElementById("label_upload").innerHTML = "<p class='text-danger'><i class='fa-solid fa-circle-xmark'></i> File too big</p>";
+            file.files[0].remove;
+        }else{
+            var formdata = new FormData();
+			formdata.append("image", file.files[0]);
+            var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+            };
+
+            fetch("http://192.168.1.11:8080/user/upload_image", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result.data)
+                document.getElementById("image-selected").style.backgroundImage = "url(http://192.168.1.11:8080/images/"+result.data+")";
+                document.getElementById("url_image_selected").setAttribute("value",result.data);
+                file.files[0].remove;
+                })
+              .catch(error => console.log('error', error));
+                }
+        }
+    }
